50556ce5ddbf9a2207bb078492b77181f005e37d
Cole Winslow <colethomaswinslow7@gmail.com>
2021-03-19 11:20:33 -0700

    added a prompt

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message:"What is the name of your Repo?"
+    }
+])
+.then(answers) => {}
