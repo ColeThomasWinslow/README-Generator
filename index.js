03bdeb90c7f08f63460a6f982397da5d30ceeb1c
Cole Winslow <colethomaswinslow7@gmail.com>
2021-03-19 22:13:50 -0700

    added prompt questions

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ inquirer.prompt([
         type: 'input',
         name: 'name',
         message:"What is the name of your Repo?"
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message:"Description of the Project?"
+    },
+    {
+        type: 'input',
+        name: 'motivation',
+        message:"Motivation for the Project?"
+    },
+    {
+        type: 'input',
+        name: 'problems',
+        message:"what problem does it solve?"
     }
 ])
-.then(answers) => {}
+// .then(answers) => {}
